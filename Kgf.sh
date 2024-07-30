77f61c3e0b902edf6fa2a9e9862c2e16fa3cde85
vipul gohel <90172796+Rdx122@users.noreply.github.com>
2024-07-30 12:24:07 +0530

    Create Kgf.sh

---
+++ Kgf.sh
@@ -0,0 +1,9 @@
+Codes: https://github.com/dkbroyt/aman/blob/main/aman.sh
+
+git clone https://github.com/dkbroy
+
+cd name.sh
+
+chmod +x name.sh && ./name.sh
+
+common: git add . && git commit -m "feat: initiated the project" && git push origin main
