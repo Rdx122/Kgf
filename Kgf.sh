331883e80d78f565abe2ece8c760344f87560143
vipul gohel <90172796+Rdx122@users.noreply.github.com>
2024-07-30 12:29:03 +0530

    Update Kgf.sh

--- Kgf.sh
+++ Kgf.sh
@@ -2,8 +2,8 @@ Codes: https://github.com/dkbroyt/aman/blob/main/aman.sh
 
 git clone https://github.com/dkbroy
 
-cd name.sh
+cd kgf.sh
 
-chmod +x name.sh && ./name.sh
+chmod +x Kgf.sh && ./Kgf.sh
 
 common: git add . && git commit -m "feat: initiated the project" && git push origin main
